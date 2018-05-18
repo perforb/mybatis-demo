f4fb17d6cbdde080887d5f17847c30247b861775
perforb <dev.perfumed.garden@gmail.com>
2018-05-18 16:35:07 +0900

    Add some projects for studying MyBatis

---
+++ mybatis-batch-processing/src/main/java/com/example/mybatisbatchprocessing/MybatisBatchProcessingApplication.java
@@ -0,0 +1,12 @@
+package com.example.mybatisbatchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisBatchProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisBatchProcessingApplication.class, args);
+	}
+}
